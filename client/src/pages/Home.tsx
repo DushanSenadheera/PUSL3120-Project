25284fd413d70668f520253b2659fbc8b35b07dd
Dushan Senadheera <89749333+DushanSenadheera@users.noreply.github.com>
2023-12-27 23:33:02 +0000

    fix: code repeat

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,42 +1,7 @@
-import React from 'react'
-import Nav from '../components/Nav/Nav'
-import Footer from '../components/Footer/Footer'
-
 function Home() {
   return (
     <div>
-        <Nav />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1>Dushanta ikmnata chemistrywa badinna labenna one</h1>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Footer/>
+      <h1>home</h1>
     </div>
   )
 }
