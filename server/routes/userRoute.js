17b383ad8eed9984dd77cbd39af6f857085c22e6
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-12-29 15:23:59 +0530

    add:password hashing

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,71 +2,85 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/userSchema');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({
-    email: email,
-    password: password
-  }).then((user) => {
+  User.findOne({ email: email }).then((user) => {
     if (user) {
-      const token = jwt.sign({
-        email: user.email,
-        fName: user.fName
-      }, process.env.JWT_KEY, {
-        expiresIn: "1h"
-      });
-      res.send({
-        token: token
+      bcrypt.compare(password, user.password).then(function (result) {
+        if (result) {
+          const token = jwt.sign({
+            email: user.email,
+            fName: user.fName
+          }, process.env.JWT_KEY, {
+            expiresIn: "1h"
+          });
+          res.send({
+            token: token,
+          });
+        } else {
+          res.status(401).send('Invalid Password');
+        }
       });
     } else {
-      res.send('user login failed');
-      req.status(401);
+      res.status(401).send('Invalid Email');
     }
-  }).catch((err) => {
-    console.log(err);
   });
 });
 
 router.post('/sign', (req, res) => {
-  const user = new User({
-    fName: req.body.fName,
-    lName: req.body.lName,
-    email: req.body.email,
-    password: req.body.password,
-    mobile: req.body.phone,
-  });
 
-  user.save().then(() => {
-    console.log('user account created');
-  }).catch((err) => {
-    console.log(err);
+  const password = req.body.password;
+  const saltRounds = 10;
+
+  bcrypt.hash(password, saltRounds).then(function (hash) {
+    hashedPassword = hash;
+
+    const user = new User({
+      fName: req.body.fName,
+      lName: req.body.lName,
+      email: req.body.email,
+      password: hashedPassword,
+      mobile: req.body.mobile,
+    });
+
+    user.save().then(() => {
+      console.log('user account created');
+    }).catch((err) => {
+      console.log(err);
+    });
   });
 });
 
 router.put('/update', (req, res) => {
-  const userId = req.body.id;
+  const email = req.body.email;
   const password = req.body.password
 
-  //find user by id and update password
-  User.findOneAndUpdate({
-    userName: userId
-  }, {
-    password: password
-  }).then(() => {
-    console.log('user password updated');
-  }).catch((err) => {
-    console.log(err);
+  bcrypt.hash(password, saltRounds).then(function (hash) {
+    hashedPassword = hash;
+
+    User.findOneAndUpdate({
+      email: email
+    }, {
+      password: password
+    }).then(() => {
+      console.log('user password updated');
+    }).catch((err) => {
+      console.log(err);
+    });
   });
+
+
 });
 
 router.delete('/delete', (req, res) => {
-  const userId = req.body.id;
+  const email = req.body.email;
 
   User.findOneAndDelete({
-    userName: userId
+    email: email
   }).then(() => {
     console.log('user profile deleted');
   }).catch((err) => {
