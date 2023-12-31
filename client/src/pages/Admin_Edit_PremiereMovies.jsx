feb654cf149b7b3d0a39c476b609cfb4bd885f9d
Dushan Senadheera <89749333+DushanSenadheera@users.noreply.github.com>
2024-01-01 02:21:07 +0530

    Revert "Admin"

--- client/src/pages/Admin_Edit_PremiereMovies.jsx
+++
@@ -1,9 +0,0 @@
-function Admin_Edit_Premiere() {
-  return (
-    <div>
-      <h1>Premiere Edit</h1>
-    </div>
-  );
-}
-
-export default Admin_Edit_Premiere;
--- client/src/pages/Admin_Edit_UpcomingMovies.jsx
+++
@@ -1,9 +0,0 @@
-function Admin_Edit_Upcoming() {
-  return (
-    <div>
-      <h1>Upcoming Edit</h1>
-    </div>
-  );
-}
-
-export default Admin_Edit_Upcoming;
