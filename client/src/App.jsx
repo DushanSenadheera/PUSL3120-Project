0902a31e0fa00c0c62438a1521366f0996cfaa35
Ashan <ashandimanthalk@gmail.com>
2024-01-05 19:00:46 +0530

    add movie descriptions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,48 +11,9 @@ import Admin_Layout from "./pages/Admin_Layout";
 import Admin_Premiere from "./pages/Admin_PremiereMovie";
 import Admin_Upcoming from "./pages/Admin_UpcomingMovie";
 import Admin_Logout from "./pages/Admin_Logout";
-<<<<<<< HEAD
-import Payment from './pages/Payment'
-import {Seatbooking} from './pages/Seatbooking/Seatbooking'
-import Description from './pages/Description'
-
-function App() {
-  return (
-   <div>
-    
-    <BrowserRouter>
-   <main>
-<Nav />   
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/siginin" element={<SignIn />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/seatbooking" element={<Seatbooking />} />
-      <Route path="/payment" element={<Payment/>} />
-      <Route path="/description" element={<Description/>} />
-    </Routes>
-    
-    <Routes>
-    <Route path="/admin" element={<Admin_Layout />}>
-          <Route path="dashboard" element={<Admin_Dashboard />} />
-          <Route path="premiere" element={<Admin_Premiere />} />
-          <Route path="bookings" element={<Admin_Booking />} />
-          <Route path="upcoming" element={<Admin_Upcoming />} />
-          <Route path="logout" element={<Admin_Logout />} />
-        </Route>
-    </Routes>
-    
- 
-   </main>
-   </BrowserRouter>
-   <Footer />
-   </div>
-   
-  )
-=======
 import Payment from "./pages/Payment";
 import { Seatbooking } from "./pages/Seatbooking/Seatbooking";
+import Description from './pages/Description'
 
 function App() {
   return (
@@ -67,9 +28,10 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/seatbooking" element={<Seatbooking />} />
             <Route path="/payment" element={<Payment />} />
+            <Route path="/description" element={<Description/>} />
           </Routes> 
 
-          {/* <Routes>
+          <Routes>
             <Route path="/" element={<Admin_Layout />}>
               <Route index element={<Admin_Dashboard />} />
               <Route path="premiere" element={<Admin_Premiere />} />
@@ -77,13 +39,12 @@ function App() {
               <Route path="upcoming" element={<Admin_Upcoming />} />
               <Route path="logout" element={<Admin_Logout />} />
             </Route>
-          </Routes> */}
+          </Routes>
         </main>
       </BrowserRouter>
       <Footer />
     </div>
   );
->>>>>>> 63c212f89d60ca9a7aa6fd774881be75253bb752
 }
 
 export default App;
